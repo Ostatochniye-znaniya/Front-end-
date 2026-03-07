c0eeb6f30eae890bb06df2d167268d4ae0fda366
Арсений <73458752+she1kopr9d@users.noreply.github.com>
2026-03-07 22:40:07 +0300

    Add checkbox

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import Button from "../components/button/Button";
-import ThemeToggle from "../components/dev/ThemeToggle";
+import CheckBox from "../components/checkbox/CheckBox";
 
 export default function Home() {
   
 
   return (
+    <div>
     <div className="flex">
-      <ThemeToggle />
       <Button title="Кнопка 1" color="btn-blue" onClick={() => alert("Кнопка 1 нажата")} />
       <Button title="Кнопка 1" color="btn-red" onClick={() => alert("Кнопка 1 нажата")} />
       <Button title="Кнопка 1" color="btn-orange" onClick={() => alert("Кнопка 1 нажата")} />  
       <Button title="Кнопка 1" color="btn-green" onClick={() => alert("Кнопка 1 нажата")} />
       <Button title="Кнопка 1" color="btn-purple" onClick={() => alert("Кнопка 1 нажата")} />
+      
+    </div>
+    <div className="flex">
+      <CheckBox text="Чек бокс с галкой" type="blue" />
+      <CheckBox text="Чек бокс с крестиком" type="red" />
+    </div>
     </div>
   );
 }
